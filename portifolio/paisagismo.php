fc17d3e0a5552859466c1c78a9fbf26802133420
Flavio Santos <flavioanalistadesistema@gmail.com>
2021-04-19 10:50:29 -0300

    Novas imagens portifolio - PAISAGISMO

--- portifolio/paisagismo.php
+++ portifolio/paisagismo.php
@@ -1,27 +1,39 @@
-
 <!DOCTYPE html>
-<html> 
+<html>
 <?php header("Content-type: text/html; charset=utf-8"); ?>
 
-<?php require_once"php/header.php";?>
+<?php require_once "php/header.php"; ?>
 
-<head><title>Paisagismo</title></head>	
+<head>
+	<title>Paisagismo</title>
+</head>
+
 <body>
 	<div class="text-center">
 		<h1>Paisagismo</h1>
+		<p class="wow lightSpeedIn" data-wow-delay="0.3s">
+			Jardins, Coberturas Vegetadas, chamadas de teto verde, Jardins verticais em muros e paredes,
+			trazem conforto térmico e acústico além de beleza, principalmente.
+			<br>
+			Temos uma forma descomplicada e acessível para traduzir a sua ideia de se relacionar com a natureza,
+			contribuir com a preservação do meio ambiente através do manejo correto de espécies e espaços,<br>
+			tornar a vida agradável e proveitosa, valorizando o seu espaço comercial, residencial, o seu condomínio,
+			as coisas simples e verdadeiramente importantes.
+		</p>
 	</div>
-<?php require_once"php/head.php";?>
-<!--Starty ** Body conteudo-->
-<?php require_once"php/body/paisagismo.php";?>	
-<!--END ** Body conteudo-->
-<div class="container-fluid">
-	<div class="text-center">
-	<blockquote><p class="rodape" >Reforma ampla de um antigo escritório que se transformou em um espaço de eventos aconchegante e estiloso</p>
-	</blockquote>
-	</div>	
-</div>
+	<?php require_once "php/head.php"; ?>
+	<!--Starty ** Body conteudo-->
+	<?php require_once "php/body/paisagismo.php"; ?>
+	<!--END ** Body conteudo-->
+	<div class="container-fluid">
+		<div class="text-center">
+			<blockquote>
+				<p class="rodape">Reforma ampla de um antigo escritório que se transformou em um espaço de eventos aconchegante e estiloso</p>
+			</blockquote>
+		</div>
+	</div>
 
-<?php require_once"php/rodape.php";?>
+	<?php require_once "php/rodape.php"; ?>
 
 	<!-- jQuery -->
 	<script src="js/jquery.min.js"></script>
@@ -37,6 +49,7 @@
 	<script src="js/salvattore.min.js"></script>
 	<!-- Main JS -->
 	<script src="js/main.js"></script>
-	
-	</body>
+
+</body>
+
 </html>
--- portifolio/php/body/paisagismo.php
+++ portifolio/php/body/paisagismo.php
@@ -4,43 +4,127 @@
 			<div id="fh5co-board" data-columns>
 				<div class="item">
 					<div class="animate-box">
-						<a href="images/portifolio/paisagismo/paisagismo_01.jpg" class="image-popup fh5co-board-img" title="Paisagismo"><img src="images/portifolio/paisagismo/paisagismo_01.jpg" alt="Free HTML5 Bootstrap template"></a>
+						<a href="images/portifolio/paisagismo/paisagismo_09.jpg" class="image-popup fh5co-board-img" title="Jardim Vertical"><img src="images/portifolio/paisagismo/paisagismo_09.jpg" alt="Jardim Vertical"></a>
 					</div>
 					<!-- <div class="fh5co-desc">Texto paisagismo 1</div> -->
 				</div>
 				<div class="item">
 					<div class="animate-box">
-						<a href="images/portifolio/paisagismo/paisagismo_02.jpg" class="image-popup fh5co-board-img" title="Paisagismo"><img src="images/portifolio/paisagismo/paisagismo_02.jpg" alt="Free HTML5 Bootstrap template"></a>
+						<a href="images/portifolio/paisagismo/paisagismo_08.jpg" class="image-popup fh5co-board-img" title="Jardim Residencial Perfeiro"><img src="images/portifolio/paisagismo/paisagismo_08.jpg" alt="Jardim Residencial Perfeiro"></a>
 						<!-- <div class="fh5co-desc">Texto paisagismo 2</div> -->
 					</div>
 				</div>
 				<div class="item">
 					<div class="animate-box">
-						<a href="images/portifolio/paisagismo/paisagismo_03.jpg" class="image-popup fh5co-board-img" title="Paisagismo"><img src="images/portifolio/paisagismo/paisagismo_03.jpg" alt="Area Livre após a reforma"></a>
+						<a href="images/portifolio/paisagismo/paisagismo_10.jpg" class="image-popup fh5co-board-img" title="Jardim Residencial Perfeiro"><img src="images/portifolio/paisagismo/paisagismo_10.jpg" alt="Jardim Residencial Perfeiro"></a>
 						<!-- <div class="fh5co-desc">Texto paisagismo 3</div> -->
 					</div>
 				</div>
 				<div class="item">
 					<div class="animate-box">
-						<a href="images/portifolio/paisagismo/paisagismo_04.jpg" class="image-popup fh5co-board-img" title="Paisagismo"><img src="images/portifolio/paisagismo/paisagismo_04.jpg" alt="Salao de festa apos a reforma"></a>
+						<a href="images/portifolio/paisagismo/paisagismo_11.jpg" class="image-popup fh5co-board-img" title="Jardim na Frente de Condomínio"><img src="images/portifolio/paisagismo/paisagismo_11.jpg" alt="Jardim na Frente de Condomínio"></a>
 						<!-- <div class="fh5co-desc">Texto paisagismo 4</div> -->
 					</div>
 				</div>
 				<div class="item">
 					<div class="animate-box">
-						<a href="images/portifolio/paisagismo/paisagismo_05.jpg" class="image-popup fh5co-board-img" title="Paisagismo"><img src="images/portifolio/paisagismo/paisagismo_05.jpg" alt="Paisagismo"></a>
+						<a href="images/portifolio/paisagismo/paisagismo_11.jpg" class="image-popup fh5co-board-img" title="Jardim em Residencial São Paulo"><img src="images/portifolio/paisagismo/paisagismo_12.jpg" alt="Jardim em Residencial São Paulo"></a>
 						<!-- <div class="fh5co-desc">Texto paisagismo 5</div> -->
 					</div>
 				</div>
 				<div class="item">
 					<div class="animate-box">
-						<a href="images/portifolio/paisagismo/paisagismo_06.jpg" class="image-popup fh5co-board-img" title="Paisagismo"><img src="images/portifolio/paisagismo/paisagismo_06.jpg" alt="Paisagismo"></a>
+						<a href="images/portifolio/paisagismo/paisagismo_13.jpg" class="image-popup fh5co-board-img" title="Jardim Humanizado"><img src="images/portifolio/paisagismo/paisagismo_13.jpg" alt="Jardim Humanizado"></a>
 						<!-- <div class="fh5co-desc">Texto paisagismo 6</div> -->
 					</div>
 				</div>
 				<div class="item">
 					<div class="animate-box">
-						<a href="images/portifolio/paisagismo/paisagismo_07.jpg" class="image-popup fh5co-board-img" title="Paisagismo"><img src="images/portifolio/paisagismo/paisagismo_07.jpg" alt="Paisagismo"></a>
+						<a href="images/portifolio/paisagismo/paisagismo_14.jpg" class="image-popup fh5co-board-img" title="Jardim com Palmeiras e Flores"><img src="images/portifolio/paisagismo/paisagismo_14.jpg" alt="Jardim com Palmeiras e Flores"></a>
+						<!-- <div class="fh5co-desc">Texto paisagismo 7</div> -->
+					</div>
+				</div>
+				<div class="item">
+					<div class="animate-box">
+						<a href="images/portifolio/paisagismo/paisagismo_15.jpg" class="image-popup fh5co-board-img" title="Jardim com Escada e Palmeiras"><img src="images/portifolio/paisagismo/paisagismo_15.jpg" alt="Jardim com Escada e Palmeiras"></a>
+						<!-- <div class="fh5co-desc">Texto paisagismo 7</div> -->
+					</div>
+				</div>
+				<div class="item">
+					<div class="animate-box">
+						<a href="images/portifolio/paisagismo/paisagismo_16.jpg" class="image-popup fh5co-board-img" title="Cerca Viva e Buxinhos em Condomínio"><img src="images/portifolio/paisagismo/paisagismo_16.jpg" alt="Cerca Viva e Buxinhos em Condomínio"></a>
+						<!-- <div class="fh5co-desc">Texto paisagismo 7</div> -->
+					</div>
+				</div>
+				<div class="item">
+					<div class="animate-box">
+						<a href="images/portifolio/paisagismo/paisagismo_18.jpg" class="image-popup fh5co-board-img" title="Calçada Meio Fio"><img src="images/portifolio/paisagismo/paisagismo_18.jpg" alt="Calçada Meio Fio"></a>
+						<!-- <div class="fh5co-desc">Texto paisagismo 7</div> -->
+					</div>
+				</div>
+				<div class="item">
+					<div class="animate-box">
+						<a href="images/portifolio/paisagismo/paisagismo_17.jpg" class="image-popup fh5co-board-img" title="Cerca Viva Condomínio"><img src="images/portifolio/paisagismo/paisagismo_17.jpg" alt="Cerca Viva Condomínio"></a>
+						<!-- <div class="fh5co-desc">Texto paisagismo 7</div> -->
+					</div>
+				</div>
+				<div class="item">
+					<div class="animate-box">
+						<a href="images/portifolio/paisagismo/paisagismo_19.jpg" class="image-popup fh5co-board-img" title="Calçada e Cerca Viva em Condomínio"><img src="images/portifolio/paisagismo/paisagismo_19.jpg" alt="Calçada e Cerca Viva em Condomínio"></a>
+						<!-- <div class="fh5co-desc">Texto paisagismo 7</div> -->
+					</div>
+				</div>
+				<div class="item">
+					<div class="animate-box">
+						<a href="images/portifolio/paisagismo/paisagismo_20.jpg" class="image-popup fh5co-board-img" title="Calçada e Cerca Viva"><img src="images/portifolio/paisagismo/paisagismo_20.jpg" alt="Calçada e Cerca Viva"></a>
+						<!-- <div class="fh5co-desc">Texto paisagismo 7</div> -->
+					</div>
+				</div>
+				<div class="item">
+					<div class="animate-box">
+						<a href="images/portifolio/paisagismo/paisagismo_21.jpg" class="image-popup fh5co-board-img" title="Calçada de Residência"><img src="images/portifolio/paisagismo/paisagismo_21.jpg" alt="Calçada de Residência"></a>
+						<!-- <div class="fh5co-desc">Texto paisagismo 7</div> -->
+					</div>
+				</div>
+				<div class="item">
+					<div class="animate-box">
+						<a href="images/portifolio/paisagismo/paisagismo_22.jpg" class="image-popup fh5co-board-img" title="Paisagismo Comercial"><img src="images/portifolio/paisagismo/paisagismo_22.jpg" alt="Paisagismo Comercial"></a>
+						<!-- <div class="fh5co-desc">Texto paisagismo 7</div> -->
+					</div>
+				</div>
+				<div class="item">
+					<div class="animate-box">
+						<a href="images/portifolio/paisagismo/paisagismo_23.png" class="image-popup fh5co-board-img" title="Plantio de Grama"><img src="images/portifolio/paisagismo/paisagismo_23.png" alt="Plantio de Grama"></a>
+						<!-- <div class="fh5co-desc">Texto paisagismo 7</div> -->
+					</div>
+				</div>
+				<div class="item">
+					<div class="animate-box">
+						<a href="images/portifolio/paisagismo/paisagismo_24.jpg" class="image-popup fh5co-board-img" title="Plantio Grama e Palmeira"><img src="images/portifolio/paisagismo/paisagismo_24.jpg" alt="Plantio Grama e Palmeira"></a>
+						<!-- <div class="fh5co-desc">Texto paisagismo 7</div> -->
+					</div>
+				</div>
+				<div class="item">
+					<div class="animate-box">
+						<a href="images/portifolio/paisagismo/paisagismo_26.png" class="image-popup fh5co-board-img" title="Plantio Manutenção de Calçada"><img src="images/portifolio/paisagismo/paisagismo_26.png" alt="Plantio Manutenção de Calçada"></a>
+						<!-- <div class="fh5co-desc">Texto paisagismo 7</div> -->
+					</div>
+				</div>
+				<div class="item">
+					<div class="animate-box">
+						<a href="images/portifolio/paisagismo/paisagismo_27.jpg" class="image-popup fh5co-board-img" title="Praça Pública - Área Verde"><img src="images/portifolio/paisagismo/paisagismo_27.jpg" alt="Praça Pública - Área Verde"></a>
+						<!-- <div class="fh5co-desc">Texto paisagismo 7</div> -->
+					</div>
+				</div>
+				<div class="item">
+					<div class="animate-box">
+						<a href="images/portifolio/paisagismo/paisagismo_28.jpg" class="image-popup fh5co-board-img" title="Terraplenagem  Casa"><img src="images/portifolio/paisagismo/paisagismo_28.jpg" alt="Terraplenagem  Casa"></a>
+						<!-- <div class="fh5co-desc">Texto paisagismo 7</div> -->
+					</div>
+				</div>
+				<div class="item">
+					<div class="animate-box">
+						<a href="images/portifolio/paisagismo/paisagismo_29.jpg" class="image-popup fh5co-board-img" title="Terraplenagem em sítio"><img src="images/portifolio/paisagismo/paisagismo_29.jpg" alt="Terraplenagem em sítio"></a>
 						<!-- <div class="fh5co-desc">Texto paisagismo 7</div> -->
 					</div>
 				</div>
